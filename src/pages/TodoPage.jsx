465193046f43927049a00db3192bf1d842f2f816
Aigul <saidillaeva saidillaevaaigul@gmail.com>
2025-03-15 13:51:52 +0600

    logout

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router";
-import { ADD_TODO, DELETE_TODO } from "../store/constants/ActionType";
+import {
+  ADD_TODO,
+  DELETE_ALL_TODOS,
+  DELETE_TODO,
+  LOGOUT,
+} from "../store/constants/ActionType";
 import { TodoForm } from "../components/TodoForm";
 import { TodoList } from "../components/TodoList";
+import styled from "styled-components";
 
 const TodoPage = () => {
   const dispatch = useDispatch();
@@ -13,7 +19,6 @@ const TodoPage = () => {
   if (!isAuth) {
     return <Navigate to="/login" />;
   }
-
   return (
     <div>
       <TodoForm onAdd={(text) => dispatch({ type: ADD_TODO, payload: text })} />
@@ -21,8 +26,20 @@ const TodoPage = () => {
         todos={todos}
         onDelete={(id) => dispatch({ type: DELETE_TODO, payload: id })}
       />
+      <Button onClick={() => dispatch({ type: DELETE_ALL_TODOS })}>
+        Delete All
+      </Button>
+      <Button onClick={() => dispatch({ type: LOGOUT })}>Logout</Button>
     </div>
   );
 };
 
+const Button = styled.button`
+  margin: 5px;
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
 export default TodoPage;
